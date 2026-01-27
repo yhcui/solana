559ac9208fddca1495958b6176090cdf0ce1c114
yhcui <cuiyuhui123@qq.com>
2026-01-27 23:47:15 +0800

    init

---
+++ 学习/Task3/blueshift_anchor_escrow/programs/blueshift_anchor_escrow/src/errors.rs
@@ -0,0 +1,12 @@
+use anchor_lang::prelude::*;
+#[error_code]
+pub enum EscrowError{
+    #[msg("Invalid amount")]
+    InvalidAmount,
+    #[msg("Invalid maker")]
+    InvalidMaker,
+    #[msg("Invalid mint a")]
+    InvalidMintA,
+    #[msg("Invalid mint b")]
+    InvalidMintB,
+}
---
+++ 学习/Task3/blueshift_anchor_escrow/programs/blueshift_anchor_escrow/src/lib.rs
@@ -0,0 +1,27 @@
+use anchor_lang::prelude::*;
+mod state;
+mod errors;
+mod instructions;
+use instructions::*;
+
+declare_id!("22222222222222222222222222222222222222222222");
+
+#[program]
+pub mod blueshift_anchor_escrow {
+    use super::*;
+
+    #[instruction(discriminator = 0)]
+    pub fn make(ctx: Context<Make>, seed:u 64, receive: u64, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    #[instruction(discriminator = 1)]
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        Ok(())
+    }
+}
+
